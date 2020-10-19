445cdbad818f1fad955f47b6ae206a4ca0949c48
eloim1996 <eloim_barbosa@hotmail.com>
2020-10-19 19:53:27 -0300

    Projeto criado

---
+++ src/main/java/br/com/tech4me/trabalhocurso/TrabalhocursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.trabalhocurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhocursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhocursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tech4me/trabalhocurso/TrabalhocursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.trabalhocurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhocursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
