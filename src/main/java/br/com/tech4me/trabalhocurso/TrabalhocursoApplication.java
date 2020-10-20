d5d633ce3c61b5377ca3312d7f14a413ff402c85
eloim1996 <eloim_barbosa@hotmail.com>
2020-10-20 12:52:03 -0300

    Criando operação de instanciação

--- src/main/java/br/com/tech4me/trabalhocurso/TrabalhocursoApplication.java
+++ src/main/java/br/com/tech4me/trabalhocurso/TrabalhocursoApplication.java
@@ -1,13 +1,32 @@
 package br.com.tech4me.trabalhocurso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TrabalhocursoApplication {
+import br.com.tech4me.trabalhocurso.domain.Categoria;
+import br.com.tech4me.trabalhocurso.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class TrabalhocursoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TrabalhocursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
