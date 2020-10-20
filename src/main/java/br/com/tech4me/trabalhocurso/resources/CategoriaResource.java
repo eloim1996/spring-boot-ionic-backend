cdee8e55541f095ae48e53357a502c4f41afbb44
eloim1996 <eloim_barbosa@hotmail.com>
2020-10-20 14:59:49 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/br/com/tech4me/trabalhocurso/resources/CategoriaResource.java
+++ src/main/java/br/com/tech4me/trabalhocurso/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tech4me.trabalhocurso.domain.Categoria;
 import br.com.tech4me.trabalhocurso.services.CategoriaService;
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -18,7 +19,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value ="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 		
