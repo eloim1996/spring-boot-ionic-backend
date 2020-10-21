bf843e25e9aebf1d0502948330d3634de7cdf2d6
eloim1996 <eloim_barbosa@hotmail.com>
2020-10-21 20:56:02 -0300

    Validação Customizada: email não repetido na atualização de clientes

---
+++ src/main/java/br/com/tech4me/trabalhocurso/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,54 @@
+package br.com.tech4me.trabalhocurso.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import br.com.tech4me.trabalhocurso.domain.Cliente;
+import br.com.tech4me.trabalhocurso.dto.ClienteDTO;
+import br.com.tech4me.trabalhocurso.repositories.ClienteRepository;
+import br.com.tech4me.trabalhocurso.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		//aqui eu vou lançar meus erros personalizados
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		for (FieldMessage e : list) {//pego cada FieldMessage e retorno os erros personalizados na lista e de erros do spring 
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
