1220c4db56e4e8b866a51ad2a76182f18c146ba1
eloim1996 <eloim_barbosa@hotmail.com>
2020-10-20 21:09:55 -0300

    Endpoint /cliente/ID, mas ainda não funciona e eu não sei pq

---
+++ src/main/java/br/com/tech4me/trabalhocurso/services/ClienteService.java
@@ -0,0 +1,25 @@
+package br.com.tech4me.trabalhocurso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.tech4me.trabalhocurso.domain.Cliente;
+
+import br.com.tech4me.trabalhocurso.repositories.ClienteRepository;
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) throws ObjectNotFoundException {
+		 Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		} 
+
+}
