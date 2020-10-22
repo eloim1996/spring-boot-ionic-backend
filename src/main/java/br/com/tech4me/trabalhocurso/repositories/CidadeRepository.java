b7bd536f005cb8c718b6900bb488b129fd2ddee7
eloim1996 <eloim_barbosa@hotmail.com>
2020-10-21 22:49:51 -0300

    Busca de produto por nome e categoria

--- src/main/java/br/com/tech4me/trabalhocurso/repositories/CidadeRepository.java
+++ src/main/java/br/com/tech4me/trabalhocurso/repositories/CidadeRepository.java
@@ -1,7 +1,6 @@
 package br.com.tech4me.trabalhocurso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import br.com.tech4me.trabalhocurso.domain.Cidade;
--- src/main/java/br/com/tech4me/trabalhocurso/repositories/ProdutoRepository.java
+++ src/main/java/br/com/tech4me/trabalhocurso/repositories/ProdutoRepository.java
@@ -1,12 +1,22 @@
 package br.com.tech4me.trabalhocurso.repositories;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.tech4me.trabalhocurso.domain.Categoria;
 import br.com.tech4me.trabalhocurso.domain.Produto;
 
 @Repository
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	
 }
