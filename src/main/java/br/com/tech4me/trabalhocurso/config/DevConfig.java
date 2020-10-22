74919f9fc5cfa62e90a8a4ae66f70dac9ae737ff
eloim1996 <eloim_barbosa@hotmail.com>
2020-10-22 12:07:36 -0300

    Implementado SmtpService com SMTP do google -- Tem que colocar um email e senha padrão nos application-properties

--- src/main/java/br/com/tech4me/trabalhocurso/config/DevConfig.java
+++ src/main/java/br/com/tech4me/trabalhocurso/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.tech4me.trabalhocurso.services.DBService;
+import br.com.tech4me.trabalhocurso.services.EmailService;
+import br.com.tech4me.trabalhocurso.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,5 +32,10 @@ public class DevConfig {
 			
 			dbService.instantiateTesteDatabase();
 			return true;
+		}
+		
+		@Bean
+		public EmailService emailService() {
+			return new SmtpEmailService();
 		}
 }
