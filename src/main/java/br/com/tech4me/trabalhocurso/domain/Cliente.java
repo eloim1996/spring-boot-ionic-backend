27805f55d1ea8ade8f2f6fbd0617505229a721b6
eloim1996 <eloim_barbosa@hotmail.com>
2020-12-15 15:13:38 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/br/com/tech4me/trabalhocurso/domain/Cliente.java
+++ src/main/java/br/com/tech4me/trabalhocurso/domain/Cliente.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,6 +20,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import br.com.tech4me.trabalhocurso.domain.enums.Perfil;
 import br.com.tech4me.trabalhocurso.domain.enums.TipoCliente;
 
 
@@ -46,6 +49,10 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@CollectionTable(name="PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
@@ -82,7 +89,7 @@ public class Cliente implements Serializable {
 
 	
 	public Cliente () {
-		
+		addPerfil(Perfil.CLIENTE);
 		
 	}
 
@@ -95,6 +102,7 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo == null) ? null : tipo.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 
@@ -186,6 +194,11 @@ public class Cliente implements Serializable {
 		this.senha = senha;
 	}
 	
+	public Set<Perfil> getPerfis(){
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
 	
-	
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
+	}
 }
--- src/main/java/br/com/tech4me/trabalhocurso/services/DBService.java
+++ src/main/java/br/com/tech4me/trabalhocurso/services/DBService.java
@@ -20,6 +20,7 @@ import br.com.tech4me.trabalhocurso.domain.PagamentoComCartao;
 import br.com.tech4me.trabalhocurso.domain.Pedido;
 import br.com.tech4me.trabalhocurso.domain.Produto;
 import br.com.tech4me.trabalhocurso.domain.enums.EstadoPagamento;
+import br.com.tech4me.trabalhocurso.domain.enums.Perfil;
 import br.com.tech4me.trabalhocurso.domain.enums.TipoCliente;
 import br.com.tech4me.trabalhocurso.repositories.CategoriaRepository;
 import br.com.tech4me.trabalhocurso.repositories.CidadeRepository;
@@ -121,18 +122,24 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
 		
 		Cliente cli1 = new Cliente(null, "Patrick","patrick_15_cunha@hotmail.com", "11111111111", TipoCliente.PESSOAFISICA, pe.encode("123"));
-		cli1.getTelefones().addAll(Arrays.asList("36411759", "12345678"));
+		cli1.getTelefones().addAll(Arrays.asList("26458895", "36452259"));
 		
 		Cliente cli2 = new Cliente(null, "Leandro","teste@teste.com", "11111111111", TipoCliente.PESSOAFISICA, pe.encode("1234"));
 		cli2.getTelefones().addAll(Arrays.asList("36411759", "12345678"));
 		
+		Cliente cli3 = new Cliente(null, "Eloim","eloim_barbosa@hotmail.com", "17407499799", TipoCliente.PESSOAFISICA, pe.encode("12345"));
+		cli3.addPerfil(Perfil.ADMIN);
+		cli3.getTelefones().addAll(Arrays.asList("36411759", "12345678"));
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli2, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli2, c2);
-		Endereco e3 = new Endereco(null, "Doutor Bretas", "285", "Parque Ingá", "Alto", "25961175", cli1, c4);
+		Endereco e3 = new Endereco(null, "Doutor Bretas", "285", "Parque Ingá", "Alto", "25961175", cli3, c4 );
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2, e3));
+		cli2.getEnderecos().addAll(Arrays.asList(e2));
+		cli3.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3));
 		enderecoRepository.saveAll(Arrays.asList(e1 , e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
